b51f3891bf5265def295707d12a2098a725aa99d
Rohitbhagat-git <105210776+Rohitbhagat-git@users.noreply.github.com>
2022-05-09 19:35:20 +0530

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,23 @@
+from flask import*
+app=Flask(__name__) #flask file
+
+@app.route("/") # "/" should always be there
+@app.route("/home")
+
+def home():
+    return "<h1>Welcome to Home Page</h1>"
+@app.route("/aboutus")
+def aboutus():
+    return "<h1>Welcome to about us page</h1>"
+@app.route("/something")
+def page2():
+    return render_template("something.html")
+@app.route("/newthing")
+def page3():
+    var="name"
+    return render_template("newthing.html",data=var)
+
+
+
+if (__name__=="__main__"):
+    app.run(debug=True) #no need to close cmd again and again
